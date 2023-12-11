0b685a587cd7a41f027297f977ff1642bbc9fee8
Hovhannes <hos.k@codemotion.us>
2023-12-12 01:46:26 +0400

    add user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,12 +1,18 @@
 from apps.extensions import db
-
+from sqlalchemy import Enum
+import enum
 
 
 class User(db.Model):
-    __tablename__ = 'results'
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer, nullable=True)
-    page = db.Column(db.Integer, nullable=True)
+    class UserRoleEnum(str, enum.Enum):
+        USER = 'user'
+        ADMIN = 'admin'
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    position = db.Column(db.String(50))
+    role = db.Column(db.Enum(UserRoleEnum), default='user', nullable=False)
